710f9d9c06f3812a9e73eb0641c937105a58d1f8
Héctor Ramos <hector@parse.com>
2012-10-17 22:00:09 -0700

    Use a UISplitViewController to show our PFQTVC of song requests.

---
+++ ios/AnySet/SongDetailViewController.m
@@ -0,0 +1,39 @@
+//
+//  SongDetailViewController.m
+//  AnySet
+//
+//  Created by Héctor Ramos on 10/17/12.
+//
+//
+
+#import "SongDetailViewController.h"
+
+@interface SongDetailViewController ()
+
+@end
+
+@implementation SongDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"Loaded detail");
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- ios/AnySet/SongRequestTableViewController.h
+++ ios/AnySet/SongRequestTableViewController.h
@@ -6,8 +6,6 @@
 //
 //
 
-#import <Parse/Parse.h>
-
 @interface SongRequestTableViewController : PFQueryTableViewController
 
 @end
