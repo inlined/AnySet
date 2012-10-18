21bee55a90af3dcacc1fd837a73f67352a723e4a
Héctor Ramos <hector@parse.com>
2012-10-17 21:30:43 -0700

    Parse iPad starter project

---
+++ ios/AnySet/SongRequestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongRequestTableViewController.h
+//  AnySet
+//
+//  Created by Héctor Ramos on 10/17/12.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface SongRequestTableViewController : PFQueryTableViewController
+
+@end
---
+++ ios/AnySet/SongRequestTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  SongRequestTableViewController.m
+//  AnySet
+//
+//  Created by Héctor Ramos on 10/17/12.
+//
+//
+
+#import "SongRequestTableViewController.h"
+
+@interface SongRequestTableViewController ()
+
+@end
+
+@implementation SongRequestTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
